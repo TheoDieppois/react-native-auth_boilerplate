f2437bd73b361ed678bd3f8851782359bc383dbf
TheoDieppois <theo76.dieppois@gmail.com>
2024-11-10 16:25:52 +0100

    feat(api): add refreshToken table to store RT hash

--- api/src/db/schema/users.ts
+++ api/src/db/schema/users.ts
@@ -1,4 +1,5 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -7,4 +8,14 @@ export const usersTable = pgTable("users", {
   role: varchar({ length: 255 }).notNull().default("user"),
 
   name: varchar({ length: 255 }).notNull(),
+});
+
+export const createUserSchema = createInsertSchema(usersTable).omit({
+  id: true,
+  role: true,
+});
+
+export const loginUserSchema = createInsertSchema(usersTable).pick({
+  email: true,
+  password: true,
 });
