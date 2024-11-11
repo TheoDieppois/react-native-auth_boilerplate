a1f81a580658412e20bfdb9b04c46fb0310cee38
TheoDieppois <theo76.dieppois@gmail.com>
2024-11-11 18:33:45 +0100

    feat(mobile): create protected page

--- mobile/app/(main)/index.tsx
+++ mobile/app/(main)/index.tsx
@@ -1,12 +1,34 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { useAuthContext } from "@/src/components/AuthProvider";
+import { useLogout } from "@/src/hooks/useLogout";
+import { Text, StyleSheet, Button } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const Index = () => {
+export default function Index() {
+  const { user } = useAuthContext();
+  const { mutate: logout } = useLogout();
+
+  const handleLogout = async () => {
+    logout();
+  };
+
   return (
-    <View>
-      <Text>Index</Text>
-    </View>
-  );
-};
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Hello {user?.name}</Text>
 
-export default Index;
+      <Button title="Sign Out" onPress={handleLogout} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 42,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
